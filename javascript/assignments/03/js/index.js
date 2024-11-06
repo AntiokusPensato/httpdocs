82374f993f6a1de2972079d567ffab236516adb1
Trevor Davis <154463993+AntiokusPensato@users.noreply.github.com>
2024-11-06 09:31:27 -0600

    Initial setup of assignment 3

---
+++ javascript/assignments/03/js/index.js
@@ -0,0 +1,39 @@
+window.onload = function () {
+
+// Assign div#page to variable
+
+
+// Assign all P elements to variable
+
+
+// Create new element UL
+
+
+// Create one for loop – loop through array of P elements
+    // In same for loop – create new element LI and place P text in LI
+    // In same for loop – Assign class names to LIs
+        // First and Last LI use class: complete
+        // use logic to determine if first or last LI
+        // do not hard code first or last LI
+        // if index value (i) is equal to 0 or index value (i) is length of array -1
+        // set class to complete
+        // All other LIs use class: cool
+    // In same for loop – count how many LIs have class: cool
+    // In same for loop – Add new LI to new UL element
+
+
+// Add UL/LIs to div#page
+
+
+// Remove original P elements from the DOM
+
+
+// Add text to the H2 element
+    // show existing H2 text followed by:
+    // Number – Items Remaining
+
+
+// Add text to TITLE element show:
+    // Number – Items Remaining
+    // followed by existing content
+};
